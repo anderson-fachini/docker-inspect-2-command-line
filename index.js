8413c0b7286df4e476c12e15074cec6f64046124
Anderson.Fachini <anderson.fachini@senior.com.br>
2016-09-21 17:49:32 -0300

    Initial version

---
+++ index.js
@@ -0,0 +1,75 @@
+var fs = require('fs')
+
+fs.readdir('.', function(err, files) {
+    if (err) {
+        console.log(err)
+    } else {
+        files.forEach(function(file) {
+            if (file.endsWith('.json')) {
+                console.log(file);
+                console.log(getDockerCommandLine(file))
+                console.log()
+            }
+        });
+    }
+});
+
+
+function getDockerCommandLine(fileName) {
+    var dockerConfig = require('./' + fileName)[0];
+    var command = 'docker run -d';
+    command += ' --name=' + dockerConfig['Name'].substring(1);
+
+    function getEnvs(dockerConfig) {
+        var ret = '';
+
+        var envs = dockerConfig['Config']['Env'];
+        if (envs != null) {
+            for (var i = 0; i < envs.length; i++) {
+                var env = envs[i];
+                if (!env.startsWith('PATH') && !env.startsWith('LANG') && !env.startsWith('LC_ALL')) {
+                    ret += ' -e ';
+                    if (env.indexOf(' ') !== -1 || env.indexOf('&') !== -1) {
+                        ret += env.substring(0, env.indexOf('=')) + '="' + env.substring(env.indexOf('=')+1) + '"';
+                    } else {
+                        ret += env;
+                    }
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    function getHostConfig(dockerConfig) {
+        var ret = '';
+
+        if (Object.keys(dockerConfig['HostConfig']['PortBindings']).length > 0) {
+            for (var port in dockerConfig['HostConfig']['PortBindings']) {
+                var bindings = dockerConfig['HostConfig']['PortBindings'][port];
+                for (var i = 0; i < bindings.length; i++) {
+                    ret += ' -p ' + bindings[i]['HostPort'] + ':' + port.split('/')[0];
+                }
+            }
+        }
+
+        if (dockerConfig['HostConfig']['Links'] != null) {
+            var links = dockerConfig['HostConfig']['Links'];
+            for (var i = 0; i < links.length; i++) {
+                var link = links[i];
+                var splitted = link.split('/');
+                ret += ' --link ' + splitted[1] + splitted[3];
+            }
+        }
+
+        return ret;
+    }
+
+
+    command += getEnvs(dockerConfig);
+    command += getHostConfig(dockerConfig);
+
+    command += ' ' + dockerConfig['Config']['Image'];
+
+    return command;
+}
